3c8d6cba016f128e322b66e23fc077cc499b5a00
Kat-exe8345 <katto.qt@gmail.com>
2026-02-01 17:08:53 +0530

    init: drizzle orm, pg setup

---
+++ src/db/index.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import * as userSchema from './schema/users';
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL ?? "",
+});
+
+export const db = drizzle(pool, { schema: { ...userSchema } });
